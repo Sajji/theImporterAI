e900c58f12e7984c89ba3bf82206b1b6eddcd0ad
sofienaz <sajji.h@gmail.com>
2023-11-23 13:09:23 -0500

    more...

--- src/importTypes.js
+++ src/importTypes.js
@@ -1,14 +1,10 @@
-const doesUUIDExist = require('./doesUUIDExist');
+const fs = require('fs');
+const targetConfig = require('../targetConfig.json');
+const doesUUIDExists = require('./doesUUIDExist.js');
 
-const domainTypes = async () => {
-    const endpoint = 'domainTypes';
-    const domainTypeUUID = '00000000-0000-0000-0000-000000003007';
-    const domainTypeExists = await doesUUIDExist(endpoint, domainTypeUUID);
-    if (domainTypeExists) {
-        console.log('Domain type exists');
-    } else {
-        console.log('Domain type does not exist');
-    }
-}
+const uniqueDomainTypesTree = JSON.parse(fs.readFileSync(`${targetConfig.backupDir}/uniqueDomainTypesTree.json`, 'utf-8'));
 
-domainTypes();
+uniqueDomainTypesTree.forEach(item => {
+    const idExists = doesUUIDExists(item.id, targetConfig);
+    console.log(`ID: ${item.id}, Exists: ${idExists}`);
+});
